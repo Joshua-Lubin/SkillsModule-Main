96df56262336e9315d4d5b23e077e0118438d926
Joshua-Lubin <jlmortonr@gmail.com>
2023-06-25 21:31:21 -0700

    Add id support to exampleFunction()
    - Add an id parameter to exampleFunction()

--- example-function.ts
+++ example-function.ts
@@ -1,6 +1,6 @@
 import { ExampleType } from "./libs/SkillsModule-Types/example-type";
 
-export const exampleFunction = (message: string): ExampleType => ({
-  id: "random id",
+export const exampleFunction = (message: string, id: string): ExampleType => ({
+  id,
   message,
 });
